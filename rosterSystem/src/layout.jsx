5bcb6c6e933be807ea1cd39400641f98ac7c0c45
JohnSey555 <hasey1261@gmail.com>
2025-08-02 18:28:37 +0700

    Fix Wrong Import DarkModeToggle

--- rosterSystem/src/layout.jsx
+++ rosterSystem/src/layout.jsx
@@ -2,7 +2,7 @@
 import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
 import { AppSidebar } from "./components/app-sidebar";
 import { Outlet } from "react-router-dom";
-import { DarkModeToggle } from "./components/darkModeToggle";
+import { DarkModeToggle } from "./components/DarkModeToggle";
 export default function Layout() {
   return (
     <SidebarProvider>
